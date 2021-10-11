2ca1a1e677570391190201376794a6a51d52cc9e
JustRelaxable <31169187+JustRelaxable@users.noreply.github.com>
2021-10-11 19:12:39 +0300

    Store Rate Button Implemented

---
+++ Assets/Scripts/StoreRatingRedirection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreRatingRedirection : MonoBehaviour
+{
+    public void OnClickRateUs()
+    {
+        Application.OpenURL("market://details?id=" + Application.identifier);
+    }
+}
