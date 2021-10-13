5e62b54808a8798df37ed2a779fa6158b03d2f3d
JustRelaxable <31169187+JustRelaxable@users.noreply.github.com>
2021-10-13 19:14:30 +0300

    Added Network Events

--- Assets/Scripts/ExtendedNetworkManager.cs
+++ Assets/Scripts/ExtendedNetworkManager.cs
@@ -17,11 +17,28 @@ public class ExtendedNetworkManager : NetworkManager
     }
     public override void OnServerDisconnect(NetworkConnection conn)
     {
+        //Called when a client disconnects from the server
         NetworkServer.DestroyPlayersForConnection(conn);
         if (conn.lastError != NetworkError.Ok)
         {
             if (LogFilter.logError) { Debug.Log("ServerDisconnected due to error: " + conn.lastError); }
         }
         Debug.Log("A client disconnected from the server: " + conn);
+    }
+
+    public override void OnStopClient()
+    {
+        //Called when client or server returns to main menu by manual
+        base.OnStopClient();
+    }
+
+    public override void OnStopHost()
+    {
+        base.OnStopHost();
+    }
+
+    public override void OnStopServer()
+    {
+        base.OnStopServer();
     }
 }
