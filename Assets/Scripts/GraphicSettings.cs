67dfc0f09110ce852300f12b31f1bb6530a4fbaf
JustRelaxable <31169187+JustRelaxable@users.noreply.github.com>
2021-10-17 21:18:22 +0300

    More Mobile Optimizations

--- Assets/Scripts/GraphicSettings.cs
+++ Assets/Scripts/GraphicSettings.cs
@@ -7,12 +7,12 @@ public class GraphicSettings : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = Screen.currentResolution.refreshRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Application.targetFrameRate = 60;
+        
     }
 }
