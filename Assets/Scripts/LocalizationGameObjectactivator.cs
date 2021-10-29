7194b4c4d299de12ccf7a98f5e9438008507ff5c
JustRelaxable <31169187+JustRelaxable@users.noreply.github.com>
2021-10-29 16:57:11 +0300

    Localization Sprite Changer Has been coded.

---
+++ Assets/Scripts/LocalizationGameObjectactivator.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+
+public class LocalizationGameObjectactivator : MonoBehaviour
+{
+    public LocalizedGameObjectSettings[] localizedGameObjectSettings;
+    private void Awake()
+    {
+        LocalizationSettings.SelectedLocaleChanged += LocalizationSettings_SelectedLocaleChanged;
+    }
+
+    private void LocalizationSettings_SelectedLocaleChanged(Locale locale)
+    {
+        for (int i = 0; i < localizedGameObjectSettings.Length; i++)
+        {
+            if(localizedGameObjectSettings[i].LocaleIdentifier == locale.Identifier)
+            {
+                localizedGameObjectSettings[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                localizedGameObjectSettings[i].gameObject.SetActive(false);
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class LocalizedGameObjectSettings
+{
+    public GameObject gameObject;
+    public SystemLanguage systemLanguage;
+    public LocaleIdentifier LocaleIdentifier { get { return new LocaleIdentifier(systemLanguage); } }
+}
